986280fac75fbd77cb1dee4ebf7f12ac87220ed1
= <=>
2022-03-17 16:36:06 +0200

    lesson_9 Context API reducers and actions - OK

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './login.css'
 
 export default function Login() {
+    const email = useRef()
+    const passwrord = useRef()
+
+    const handleCLick = (e) => { 
+        e.preventDefault()          
+        console.log(email.current.value)       
+    }
+
     return (
         <div className='login'> 
             <div className="loginWrapper">
@@ -10,13 +18,24 @@ export default function Login() {
                     <span className="loginDesc">Connect with friends all over the world on Coolsite</span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder='Email' className="loginInput" />
-                        <input placeholder='Password' className="loginInput" />
+                    <form className="loginBox" onSubmit={handleCLick}>
+                        <input 
+                            placeholder='Email' 
+                            type='email'
+                            required 
+                            className="loginInput" 
+                            ref={email}/>
+                        <input 
+                            placeholder='Password' 
+                            type='password' 
+                            required
+                            minLength='6'
+                            className="loginInput" 
+                            ref={passwrord}/>
                         <button className="loginButton">Log In</button>
                         <span className="loginForgot">Forgot password?</span>
                         <button className="loginRegisterButton">Create an account</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
